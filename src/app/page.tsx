66d5a9e28ccc6902c0abdce72cc59ecafa616093
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-16 02:50:11 +0000

    reduce the time spent parsing, compiling and executing JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { QuoteCalculator } from '@/components/landing/quote-calculator';
+import { ClientQuoteCalculator } from '@/components/landing/client-quote-calculator';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center min-h-screen p-4 md:p-8">
       <div className="w-full mx-auto">
-        <QuoteCalculator />
+        <ClientQuoteCalculator />
       </div>
     </main>
   );
